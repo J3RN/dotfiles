6edb9ecd041d9cc53250bc75468cc7531c0094a5
J3RN <jonarnett90@gmail.com>
2015-01-17 23:11:01 -0500

    Removed bundler plugin from fish

--- config.fish
+++ config.fish
@@ -7,7 +7,7 @@ set fish_theme bobthefish
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
 # Example format: set fish_plugins autojump bundler
-set fish_plugins rvm git rails bundler emoji-clock extract vi-mode
+set fish_plugins rvm git rails emoji-clock extract vi-mode
 
 # Path to your custom folder (default path is $FISH/custom)
 #set fish_custom $HOME/dotfiles/oh-my-fish
