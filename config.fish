77b454e4b183b4de7bfa1bdaa169f9751eb9916f
Jonathan Arnett <jonarnett90@gmail.com>
2015-03-31 14:57:20 -0400

    Removed RVM references

--- config.fish
+++ config.fish
@@ -4,13 +4,11 @@ set fish_path $HOME/.oh-my-fish
 # Theme
 set fish_theme bobthefish
 
-# Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
-# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
-# Example format: set fish_plugins autojump bundler
-set fish_plugins rvm git rails emoji-clock extract vi-mode
-
 # Path to your custom folder (default path is $FISH/custom)
 #set fish_custom $HOME/dotfiles/oh-my-fish
+
+# Load Plugins
+set fish_plugins git rails emoji-clock extract vi-mode
 
 # Load aliases
 . ~/.fish_aliases
