efe45388b1ae9daf52555314d7b95273fda6f052
Jonathan Arnett <jonarnett90@gmail.com>
2016-09-09 10:38:43 -0400

    Added my .emacs.d directory

---
+++ .emacs.d/elpa/ruby-test-mode-1.7/ruby-test-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "ruby-test-mode" "1.7" "Minor mode for Behaviour and Test Driven" '((ruby-mode "1.0") (pcre2el "1.8")))
---
+++ .emacs.d/elpa/znc-20160627.1332/znc-autoloads.el
@@ -0,0 +1,26 @@
+;;; znc-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "znc" "znc.el" (22468 62897 0 0))
+;;; Generated autoloads from znc.el
+
+(autoload 'znc-erc "znc" "\
+Connect to a configured znc network
+
+\(fn &optional NETWORK)" t nil)
+
+(autoload 'znc-all "znc" "\
+Connect to all known networks
+
+\(fn &optional DISCONNECT)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; znc-autoloads.el ends here
