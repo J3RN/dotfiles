b8b90e35d05b01438d69e9b718c51cb1979abe00
Jonathan Arnett <j3rn@j3rn.com>
2021-12-06 12:12:26 -0500

    Configure Elixir editor env vars

--- bash/.bashrc.d/elixir.sh
+++ bash/.bashrc.d/elixir.sh
@@ -1,3 +1,7 @@
 if [ -f $HOME/elixir-ls ]; then
     export PATH=$HOME/elixir-ls:$PATH
 fi
+
+export ELIXIR_EDITOR="emacsclient +__LINE__ __FILE__"
+export PLUG_EDITOR=$ELIXIR_EDITOR
+export ECTO_EDITOR=$ELIXIR_EDITOR
