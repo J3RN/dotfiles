e462671719a835977f65c146c33e1f43718769cd
Jonathan Arnett <jonarnett90@gmail.com>
2023-08-15 22:02:06 -0400

    Have reify detect default branch (instead of assuming "main")

--- bash/.bashrc.d/reify.sh
+++ bash/.bashrc.d/reify.sh
@@ -1,6 +1,7 @@
 function reify() {
   repo="git@github.com:$1/$2"
-  target="$HOME/Code/$1/$2/main"
+  branch_name=$(git ls-remote --symref $repo HEAD | awk -F'[/\t]' 'NR == 1 {print $3}')
+  target="$HOME/Code/github.com/$1/$2/$branch_name"
   git clone $repo $target
   cd $target
 }
