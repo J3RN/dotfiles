b7b10d7ee88f8729d3529236d99003fc612364a9
Jonathan Arnett <j3rn@j3rn.com>
2023-09-09 14:26:21 -0400

    Remove the "github.com" prefix from reify
    A repo doesn't "belong" to GitHub, even if it is hosted there.  Some
    repos may have remotes on different forges.  This naming scheme
    implied that a repo belonged to a forge (specifically GitHub) and
    therefore was mistaken.

--- bash/.bashrc.d/reify.sh
+++ bash/.bashrc.d/reify.sh
@@ -1,7 +1,7 @@
 function reify() {
   repo="git@github.com:$1/$2"
   branch_name=$(git ls-remote --symref $repo HEAD | awk -F'[/\t]' 'NR == 1 {print $3}')
-  target="$HOME/Code/github.com/$1/$2/$branch_name"
+  target="$HOME/Code/$1/$2/$branch_name"
   git clone $repo $target
   cd $target
 }
