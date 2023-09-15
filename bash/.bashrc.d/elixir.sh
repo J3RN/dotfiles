fe6a03a6fbb447bf3c0f6aa069823777d01aafff
Jonathan Arnett <j3rn@j3rn.com>
2023-09-15 14:10:44 -0400

    Add Distrobox Elixir workaround

--- bash/.bashrc.d/elixir.sh
+++ bash/.bashrc.d/elixir.sh
@@ -1,3 +1,6 @@
 export ELIXIR_EDITOR="emacsclient +__LINE__ __FILE__"
 export PLUG_EDITOR=$ELIXIR_EDITOR
 export ECTO_EDITOR=$ELIXIR_EDITOR
+
+# Workaround for Distrobox not setting locale properly
+export ELIXIR_ERL_OPTIONS="+fnu"
