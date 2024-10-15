36af139e601e72cdb0278fc6df88d939bfafaee9
Jonathan Arnett <j3rn@j3rn.com>
2024-10-15 14:09:47 -0400

    Updates for Ubuntu's crumby Bash

--- bash/.bashrc.d/02_prompt.sh
+++ bash/.bashrc.d/02_prompt.sh
@@ -9,7 +9,11 @@ cdpwdhook () {
   fi
 }
 
-export PROMPT_COMMAND="$PROMPT_COMMAND;cdpwdhook"
+if test -z "$PROMPT_COMMAND"; then
+    export PROMPT_COMMAND="cdpwdhook"
+else
+    export PROMPT_COMMAND="$PROMPT_COMMAND;cdpwdhook"
+fi
 
 if hash starship 2> /dev/null && [[ $TERM != "dumb" ]]; then
     eval "$(starship init bash)"
