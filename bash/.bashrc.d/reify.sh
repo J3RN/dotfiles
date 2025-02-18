0f9dcd515809be2c997ae2152d5f58cf48ee7766
Jonathan Arnett <j3rn@j3rn.com>
2025-02-17 21:18:43 -0500

    Don't (attempt to) clobber with reify

--- bash/.bashrc.d/reify.sh
+++ bash/.bashrc.d/reify.sh
@@ -1,7 +1,10 @@
 function reify() {
-  repo="git@github.com:$1/$2"
-  branch_name=$(git ls-remote --symref $repo HEAD | awk -F'[/\t]' 'NR == 1 {print $3}')
-  target="$HOME/Code/$1/$2/$branch_name"
-  git clone $repo $target
-  cd $target
+    local repo="git@github.com:$1/$2"
+    local branch_name=$(git ls-remote --symref $repo HEAD | awk -F'[/\t]' 'NR == 1 {print $3}')
+    local target="$HOME/Code/$1/$2/$branch_name"
+
+    if [ ! -d $target ]; then
+        git clone $repo $target
+    fi
+    cd $target
 }
