07adb412a347e0674aa140fd250b2a3f045a63ed
Jonathan Arnett <j3rn@j3rn.com>
2025-07-12 20:22:50 -0400

    exa -> eza; icons always

--- bash/.bashrc.d/ls.sh
+++ bash/.bashrc.d/ls.sh
@@ -1,7 +1,7 @@
 if type lsd &> /dev/null; then
     alias ls="lsd -lAh --group-dirs=first"
-elif type exa &> /dev/null; then
-    alias ls="exa -la --group-directories-first --git"
+elif type eza &> /dev/null; then
+    alias ls="eza -la --group-directories-first --git --icons=always"
 elif [ $(uname -s) = "Linux" ]; then
     alias ls="ls -lAh --color=auto --group-directories-first"
 else
