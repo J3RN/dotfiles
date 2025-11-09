a40ca84b88c2129c8dc0019d74d8105144e12f3b
Jonathan Arnett <j3rn@j3rn.com>
2025-11-09 11:40:55 -0500

    Support additional forges with reify

--- bash/.bashrc.d/reify.sh
+++ bash/.bashrc.d/reify.sh
@@ -1,6 +1,17 @@
 function reify() {
-    local repo="git@github.com:$1/$2"
+    local repo
+
+    case $3 in
+        "" | github) repo="git@github.com:$1/$2" ;;
+        codeberg) repo="git@codeberg.org/$1/$2" ;;
+        *) >&2 echo "Unknown forge '$3'" && return 1 ;;
+    esac
+
     local branch_name=$(git ls-remote --symref $repo HEAD | awk -F'[/\t]' 'NR == 1 {print $3}')
+    if [ -z $branch_name ]; then
+        return 1
+    fi
+
     local target="$HOME/Code/$1/$2/$branch_name"
 
     if [ ! -d $target ]; then
