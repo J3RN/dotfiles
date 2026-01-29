08b2a875d4a60bcd4c9c733a90495850bc870607
Jonathan Arnett <j3rn@j3rn.com>
2026-01-29 10:00:54 -0500

    Add escripts to PATH if dir exists

--- bash/.bashrc.d/elixir.sh
+++ bash/.bashrc.d/elixir.sh
@@ -2,5 +2,9 @@ export ELIXIR_EDITOR="emacsclient +__LINE__ __FILE__"
 export PLUG_EDITOR=$ELIXIR_EDITOR
 export ECTO_EDITOR=$ELIXIR_EDITOR
 
+if [ -d "$HOME/.mix/escripts" ]; then
+  export PATH="$HOME/.mix/escripts:$PATH"
+fi
+
 # Workaround for Distrobox not setting locale properly
 export ELIXIR_ERL_OPTIONS="+fnu"
