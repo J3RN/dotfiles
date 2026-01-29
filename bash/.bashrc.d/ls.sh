4737347ed779688124ca269696aa5074612799ee
Jonathan Arnett <j3rn@j3rn.com>
2026-01-29 10:26:55 -0500

    Use -h option with eza for headers

--- bash/.bashrc.d/ls.sh
+++ bash/.bashrc.d/ls.sh
@@ -1,7 +1,7 @@
 if type lsd &> /dev/null; then
     alias ls="lsd -lAh --group-dirs=first"
 elif type eza &> /dev/null; then
-    alias ls="eza -la --group-directories-first --git --icons=always"
+    alias ls="eza -lah --group-directories-first --git --icons=always"
 elif [ $(uname -s) = "Linux" ]; then
     alias ls="ls -lAh --color=auto --group-directories-first"
 else
