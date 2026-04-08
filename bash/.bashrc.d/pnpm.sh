f58a0ce667ea152749b99cd9fd2ae3a62a303d43
Jonathan Arnett <j3rn@j3rn.com>
2026-04-08 10:30:46 -0400

    Use $HOME with $PNPM_HOME

--- bash/.bashrc.d/pnpm.sh
+++ bash/.bashrc.d/pnpm.sh
@@ -1,4 +1,4 @@
-export PNPM_HOME="/var/home/j3rn/.local/share/pnpm"
+export PNPM_HOME="$HOME/.local/share/pnpm"
 case ":$PATH:" in
     *":$PNPM_HOME:"*) ;;
     *) export PATH="$PNPM_HOME:$PATH" ;;
